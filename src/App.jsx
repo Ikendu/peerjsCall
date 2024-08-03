186bc4dd575658ea768d06b53bae7e43ef032193
Aniede Chibundu <ndubest56@gmail.com>
2024-08-03 11:01:24 +0100

    peerjs call app

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,29 @@ function App() {
     peer.on(`open`, (id) => {
       setPeerId(id);
     });
+
+    //Answer the call
+
+    peer.on("call", function (call) {
+      let getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+      getUserMedia(
+        { video: true, audio: true },
+        function (stream) {
+          call.answer(stream); // Answer the call with an A/V stream.
+
+          call.on("stream", function (remoteStream) {
+            // Show stream in some video/canvas element.
+          });
+        },
+        function (err) {
+          console.log("Failed to get local stream", err);
+        }
+      );
+    });
   }, []);
 
   // for making calls
@@ -31,14 +54,12 @@ function App() {
           // Show stream in some video/canvas element.
           remoteVideoRef.current.srcObject = remoteStream;
         });
-      },
-      function (err) {
-        console.log("Failed to get local stream", err);
       }
+      // function (err) {
+      //   console.log("Failed to get local stream", err);
+      // }
     );
   };
-
-  // for anserwering calls
 
   return (
     <div>
